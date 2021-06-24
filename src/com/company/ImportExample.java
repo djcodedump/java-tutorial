8f35cd79e3641b6da049f0d0b2dfb8f7e31b564e
djcodedump <djcodedump@gmail.com>
2021-06-25 01:53:58 +0530

    Initial Commit

---
+++ src/com/company/ImportExample.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Date;
+import java.util.Random;
+
+public class ImportExample {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+        Date date;
+    }
+}
