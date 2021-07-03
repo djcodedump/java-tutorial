f621d9d91443232ac1cea9403ee44397a6b732ed
djcodedump <djcodedump@gmail.com>
2021-07-04 01:22:30 +0530

    adding a README.md

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,13 @@ package com.company;
 
 public class Main {
 
+    private String s;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int num = Integer.MAX_VALUE+1;
+        System.out.println(num);
+
     }
 }
