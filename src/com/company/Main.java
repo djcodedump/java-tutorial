8b3585ed83d567fd0871a4a576d874863316bc63
djcodedump <djcodedump@gmail.com>
2021-07-04 12:26:49 +0530

    modified

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        int num = Integer.MAX_VALUE+1;
-        System.out.println(num);
-
     }
 }
