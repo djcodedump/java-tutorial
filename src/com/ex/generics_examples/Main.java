f18c860bbf341abde83137732af80492b7a19735
djcodedump <djcodedump@gmail.com>
2021-07-10 23:29:41 +0530

    modified

--- src/com/ex/generics_examples/Main.java
+++ src/com/ex/generics_examples/Main.java
@@ -4,46 +4,6 @@ import java.io.Serializable;
 import java.sql.Array;
 import java.util.*;
 
-class Container<T extends Number> {
-    private T value;
-
-    public T getValue() {
-        return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
-    }
-
-    public void demo(ArrayList<? extends T> list){
-
-    }
-
-    public void demo2(ArrayList<? super T> list){
-
-    }
-
-    public <Z> Z noOp(Z val){
-        return  val;
-    }
-
-    /*
-    To get data from (list.get) > use extends
-    To put data into (list.add) -> use super
-     */
-    public void  copy(List<? extends T> src, List<? super T> dest){
-
-    }
-
-    /*
-    <?> is a raw type, it basically means <? extends Object>
-     */
-    public void reverse (List<?> src){
-
-    }
-
-}
-
 
 public class Main {
 
