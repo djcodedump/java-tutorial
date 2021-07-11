b6c389f05c75f06e0f66e2807d61549fe708943e
djcodedump <djcodedump@gmail.com>
2021-07-11 19:59:59 +0530

    modified

--- src/com/ex/generics_examples/Container.java
+++ src/com/ex/generics_examples/Container.java
@@ -1,44 +1,93 @@
 package com.ex.generics_examples;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Container<T extends Number> {
-    private T value;
+/**
+Generic Type Class or Interface
+A class is generic if it declares one or more type variables
+Scope of the generic type is within the class
+These type variables are known as the type parameters of the class
 
-    public T getValue() {
-        return value;
+Once initialized the class with a certain type, class should be used
+with that particular type only
+ **/
+public class Container<T, C> {
+    private T val;
+    private C name;
+
+    //cannot have a static generic type field
+    //private static T member;
+
+    public Container(T val, C name) {
+        this.val = val;
+        this.name = name;
     }
 
-    public void setValue(T value) {
-        this.value = value;
+    /*
+    A generic constructor is a constructor that introduces its own type parameters
+    A non generic class can have a generic constructor
+    */
+    public <Z> Container(Z other){
+        //this.value = other;
     }
 
-    public void demo(ArrayList<? extends T> list) {
-
-    }
-
-    public void demo2(ArrayList<? super T> list) {
-
-    }
-
-    public <Z> Z noOp(Z val) {
+    public T getVal() {
         return val;
     }
 
-    /*
-    To get data from (list.get) > use extends
-    To put data into (list.add) -> use super
-     */
-    public void copy(List<? extends T> src, List<? super T> dest) {
-
+    public void setVal(T val) {
+        this.val = val;
     }
 
     /*
-    <?> is a raw type, it basically means <? extends Object>
+    A generic method can introduce its own type parameters.
+    the type parameter's scope is limited to the method where it is declared.
+     */
+    public <K, V> boolean equality(Pair<K, V> pair1, Pair<K, V> pair2){
+        return pair1.getValue().equals(pair2.getValue());
+    }
+
+    /* wildcard (?), represents an unknown type
+    <?> is a raw type, an unbounded wildcard, it basically means <? extends Object>
      */
     public void reverse(List<?> src) {
 
     }
+
+    /*
+    Bounded wildcard put some restrictions over possible types
+
+    <? extends Number> defines the upper bound to be Number,
+    so it includes Number and all its subclasses
+    <? super Number> defines the lower bound to be Number,
+    so it includes Number and all its superclasses
+
+    To get data from (list.get) > use extends (upper bound)
+    To put data into (list.add) -> use super (lower bound)
+     */
+    public void copy(List<? extends Number> src, List<? super Number> dest) {
+
+    }
+
+    //multiple bounds
+    public <M extends T, C> void tryThis(M obj1){
+
+    }
+
+    public static void main(String[] args) {
+
+        Container<Number, String> con = new Container<>("my container");
+
+        Pair<Integer, String> pair1 = new Pair<>(1, "name1");
+        Pair<Integer, String> pair2 = new Pair<>(2, "name2");
+
+        con.<Integer, String>equality(pair1,pair2);
+        con.equality(pair1,pair2);//with type inference
+    }
+
+    //Generic class cannot extend Throwable
+    //class MyException<T> extends Exception{ }
 
 }
--- src/com/ex/generics_examples/Main.java
+++ src/com/ex/generics_examples/Main.java
@@ -1,27 +1,73 @@
 package com.ex.generics_examples;
 
-import java.io.Serializable;
-import java.sql.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+/**
+Java Generics are a language feature that allows for definition
+Generics enable types (classes and interfaces) to be parameters
+when defining classes, interfaces and methods
+Type parameters provide a way for you to re-use the same code with different inputs
 
+Generics force type safety in java language. Without this type of safety,
+the code could have infected by various bugs that get revealed only in runtime
+avoid {@link ClassCastException}
+
+An invocation of a generic type is generally known as a parameterized type
+Generic types are instantiated to form parameterized types by providing
+actual type arguments that replace the formal type parameters.
+LinkedList<E> is a generic type
+LinkedList<Integer> is a parameterized type
+
+The T in Foo<T> is a type parameter and the String in Foo<String> is a type argument
+
+Generics not compatible with primitives
+You cannot write: List<int> lst = new ArrayList<>();
+
+Arrays do not support generics, because arrays are covariant
+.i.e. an array of supertype references is a supertype of an
+ array of subtype references
+ Object[] is a supertype of String[]
+ But collections are invariant
+ List<Object> is not a supertype of List<String>
+
+Conventions:
+ E - Element (used extensively by the Java Collections Framework)
+ K - Key
+ N - Number
+ T - Type
+ V - Value
+ S,U,V etc. - 2nd, 3rd, 4th types
+
+In the Collections framework, generics add this type checking of parameters
+in the code at compile-time, automatically
+
+'type erasure' essentially means that all the extra information added using
+generics into source code will be removed from bytecode generated from it
+
+**/
 public class Main {
 
     public static void main(String[] args) {
-        test1();
-        test2();
+        test();
     }
 
-    static void test1(){
-        Container<Number> con = new Container<>();
 
-        con.demo(new ArrayList<Integer>());
+    static void test(){
 
-        con.demo2(new ArrayList<Serializable>());
+        List<Object> objlst = new ArrayList<>();
+        List<String> strlst = new ArrayList<>();
 
-    }
+        //objlst = strlst; not possible, invariants
 
-    static void test2(){
+        Object[] objArr = new Object[5];
+        String[] strArr = new String[5];
+
+        objArr = strArr;//possible, covariants
+
+
         List<Integer> list1 = Arrays.asList(123,321,542);
 
         Collections.sort(list1, (o1, o2) -> (o1%10 - o2%10));
@@ -35,6 +81,6 @@ public class Main {
             add(345);
         }};
 
-        list2.forEach(x -> System.out.println(x));
+        list2.forEach(System.out::println);
     }
 }
